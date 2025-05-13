1fc85bc66e817980ddc955b96ef37d49e40193d7
medanmilos1831 <medanmilos1831@gmail.com>
2025-05-13 14:35:40 +0200

    init

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,9 @@
-// src/index.ts
-console.log("Hello, TypeScript!");
-export const person = "Milos";
+import { EventManager } from './EventManager';
+
+let manager = new EventManager();
+const dispatch = manager.managerAction;
+const subscribe = manager.managerSubscribe;
+const eventInterceptor = manager.managerEventInterceptor;
+const configEventManager = manager.configEventManager;
+const logging = manager.logging;
+export { dispatch, eventInterceptor, subscribe, configEventManager, logging };
