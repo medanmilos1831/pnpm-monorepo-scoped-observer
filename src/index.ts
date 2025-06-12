137e7623601c049061073d00b607e3f1af045709
medanmilos1831 <medanmilos1831@gmail.com>
2025-06-12 12:46:41 +0200

    createEventManager

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,25 @@
-import { EventManager } from './EventManager';
+import { EventEntity } from "./EventEntity";
+import { EventManager } from "./EventManager";
+import { scopeNodeType } from "./types";
 
-let manager = new EventManager();
-const dispatch = manager.managerAction;
-const subscribe = manager.managerSubscribe;
-const eventInterceptor = manager.managerEventInterceptor;
-const configEventManager = manager.configEventManager;
-const logging = manager.logging;
-export { dispatch, eventInterceptor, subscribe, configEventManager, logging };
+export const createEventManager = (arr: scopeNodeType[]) => {
+  let manager = new EventManager();
+  const buildScopes = (arr: scopeNodeType[]) => {
+    const obj: { [key: string]: EventEntity } = {};
+    arr.forEach(({ scope, subScopes, log = false }) => {
+      const entity = new EventEntity(scope, log);
+      if (subScopes) {
+        entity.scopedEvents = buildScopes(subScopes) as any;
+      }
+      obj[scope] = entity;
+    });
+    return obj;
+  };
+  manager.events = buildScopes(arr);
+  return {
+    dispatch: manager.managerAction,
+    subscribe: manager.managerSubscribe,
+    eventInterceptor: manager.managerEventInterceptor,
+    logging: manager.logging,
+  };
+};
