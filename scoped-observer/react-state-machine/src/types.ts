a031e907d7290b1474697640a03c7ae786fcd4b5
medanmilos1831 <medanmilos1831@gmail.com>
2025-08-16 23:11:39 +0200

    update state machine

--- scoped-observer/react-state-machine/src/types.ts
+++ scoped-observer/react-state-machine/src/types.ts
@@ -1,7 +1,5 @@
-import { IScopedObserver } from '../scoped-observer';
-
-export const MACHINE_EVENT = 'machineEvent';
-export const MACHINE_SCOPE = 'machineScope';
+export const MACHINE_EVENT = "machineEvent";
+export const MACHINE_SCOPE = "machineScope";
 
 export type TransitionMap<S extends string, T extends string = string> = {
   [K in S]: {
