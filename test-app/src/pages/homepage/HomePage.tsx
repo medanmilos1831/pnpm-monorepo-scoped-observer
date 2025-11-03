152b575bbe14bfb03125e0a54fb0ecd61929791c
medanmilos1831 <medanmilos1831@gmail.com>
2025-11-03 11:12:21 +0100

    Add separate HomePage components for wizard and scrollium demos

--- test-app/src/pages/homepage/HomePage.tsx
+++ test-app/src/pages/homepage/HomePage.tsx
@@ -1,37 +1,11 @@
-import { WizControls } from "../../components/Controls";
-import { SomeComponent } from "../../components/SomeComponent";
-import { StepFour } from "../../components/StepFour";
-import { StepOne } from "../../components/StepOne";
-import { StepThree } from "../../components/StepThree";
-import { StepTwo } from "../../components/StepTwo";
-import { WizNavigation } from "../../components/WizNavigation";
-import { useWizard, Wizard } from "../../wizService";
+import { HomePageScroll } from "./HomePageScroll";
+import { HomePageWizard } from "./HomePageWizard";
 
-const WizBody = () => {
-  const StepMap: any = {
-    stepOne: StepOne,
-    stepTwo: StepTwo,
-    stepThree: StepThree,
-    stepFour: StepFour,
-  };
-  const { activeStep } = useWizard();
-  const StepComponent = StepMap[activeStep];
-  return <StepComponent />;
-};
 const HomePage = () => {
   return (
     <>
-      <h1>HomePage</h1>
-      <SomeComponent />
-      <Wizard
-        id="wizard-1"
-        steps={["stepOne", "stepTwo", "stepThree"]}
-        activeStep="stepOne"
-      >
-        <WizNavigation />
-        <WizBody />
-        <WizControls />
-      </Wizard>
+      {/* <HomePageWizard /> */}
+      <HomePageScroll />
     </>
   );
 };
---
+++ test-app/src/pages/homepage/HomePageScroll.tsx
@@ -0,0 +1,33 @@
+import { Scroll } from "../../scroll";
+
+const HomePageScroll = () => {
+  return (
+    <>
+      <div>
+        <h1>Home Page Scroll</h1>
+      </div>
+      <div style={{ height: "10rem" }}>
+        <Scroll
+          id="scroll-1"
+          axis="vertical"
+          onScroll={(payload) => {
+            console.log("scroll", payload);
+          }}
+        >
+          {new Array(50).fill(0).map((_, index) => (
+            <div
+              key={index}
+              style={{
+                backgroundColor: index % 2 === 0 ? "red" : "blue",
+              }}
+            >
+              <h1>Item {index}</h1>
+            </div>
+          ))}
+        </Scroll>
+      </div>
+    </>
+  );
+};
+
+export { HomePageScroll };
