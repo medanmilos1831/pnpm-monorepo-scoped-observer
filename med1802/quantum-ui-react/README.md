6ebf8b7deb6d748ef75082720a270d8bd15433ca
medanmilos1831 <medanmilos1831@gmail.com>
2025-11-19 09:00:51 +0100

    feat: add quantum-ui-react package to workspace

---
+++ med1802/quantum-ui-react/README.md
@@ -0,0 +1,1 @@
+# 🔄 Quantum UI React
---
+++ med1802/quantum-ui-react/src/index.ts
@@ -0,0 +1,44 @@
+import { useEffect, useState, useSyncExternalStore } from "react";
+import { framework } from "@med1802/quantum-ui";
+
+const quantumUiReact = (props: ReturnType<typeof framework.createModule>) => {
+  return {
+    useModelSelector: (modelId: string) => {
+      const [mount] = useState(() => {
+        return (notify: () => void) => {
+          return props.onModelMount(modelId, () => {
+            notify();
+          });
+        };
+      });
+      const [unmount] = useState(() => {
+        return (notify: () => void) => {
+          return props.onModelUnmount(modelId, () => {
+            notify();
+          });
+        };
+      });
+      const [snapshot] = useState(() => () => {
+        return props.getModelById(modelId);
+      });
+      let value = undefined;
+      value = useSyncExternalStore(mount, snapshot);
+      value = useSyncExternalStore(unmount, snapshot);
+      return value;
+    },
+    useCreateModel: (modelId: string) => {
+      props.createModel(props);
+
+      useEffect(() => {
+        return () => {
+          props.removeModel(modelId);
+        };
+      }, []);
+    },
+    getModelById: props.getModelById,
+    createModel: props.createModel,
+    removeModel: props.removeModel,
+  };
+};
+
+export { quantumUiReact };
