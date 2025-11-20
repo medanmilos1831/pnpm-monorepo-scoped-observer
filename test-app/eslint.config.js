e16303b7979a36da2069057d80998777be854952
medanmilos1831 <medanmilos1831@gmail.com>
2025-11-20 08:40:47 +0100

    feat: add test app and adjust eslint

---
+++ test-app/eslint.config.js
@@ -0,0 +1,9 @@
+import { defineConfig, globalIgnores } from 'eslint/config'
+
+export default defineConfig([
+  globalIgnores(['dist']),
+  {
+    files: ['**/*.{ts,tsx}'],
+    rules: {},
+  },
+])
