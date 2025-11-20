c20a58c06ebb30a161e23efcc8bccecdf11277c6
medanmilos1831 <medanmilos1831@gmail.com>
2025-11-20 09:57:36 +0100

    feat: update test app toggle demo

--- test-app/src/pages/HomePage.tsx
+++ test-app/src/pages/HomePage.tsx
@@ -1,95 +1,62 @@
-import { quantumUiReact } from "@med1802/quantum-ui-react";
-import { useEffect, useSyncExternalStore } from "react";
-const quantumUi = quantumUiReact();
-interface ToggleState {
-  status: string;
-}
-interface ToggleMutations {
-  setStatus: (status: string) => void;
-}
-interface ToggleGetters {
-  getStatus: () => string;
-}
-interface ToggleApiClient {
-  commands: {
-    toggle: () => void;
-  };
-  subscribers: {
-    onChange: (callback: (payload: string) => void) => () => void;
-  };
-  getStatus: () => string;
-}
-const toggleModule = quantumUi.createModule<
-  ToggleState,
-  ToggleMutations,
-  ToggleGetters,
-  ToggleApiClient
->({
-  name: "toggle",
-  model: (props: { id: string }) => {
-    return {
-      id: props.id,
-      state: {
-        status: "on",
-      },
-      mutations(state) {
-        return {
-          setStatus: (status: string) => {
-            state.status = status;
-          },
-        };
-      },
-      getters(state) {
-        return {
-          getStatus: () => state.status,
-        };
-      },
-    };
-  },
-  modelClient: (model, broker) => {
-    return {
-      commands: {
-        toggle: () => {
-          model.mutations.setStatus(
-            model.getters.getStatus() === "on" ? "off" : "on"
-          );
-          broker.publish({
-            eventName: "onChange",
-            payload: model.getters.getStatus(),
-          });
-        },
-      },
-      subscribers: {
-        onChange: (callback: (payload: string) => void) => {
-          return broker.subscribe({
-            eventName: "onChange",
-            callback,
-          });
-        },
-      },
-      getStatus: model.getters.getStatus,
-    };
-  },
-});
-const HomePage = () => {
-  const modelSelector = toggleModule.useModelSelector("toggle");
-  toggleModule.useCreateModel({ id: "toggle", initState: "on" });
-  const model = toggleModule.getModelById("toggle");
-  const visibility = useSyncExternalStore(
-    model.subscribers.onChange,
-    model.getStatus
+import { Button, Modal } from "antd";
+import { useEffect } from "react";
+import { createToggleClient } from "@med1802/quantum-toggle";
+
+const { useToggle, useToggleCommands, useToggleSelector } =
+  createToggleClient();
+
+const UserModal = ({ model }: { model: any }) => {
+  const visibility = useToggle(model);
+  const { onClose } = useToggleCommands(model.id);
+
+  return (
+    <Modal open={visibility === "on"} onCancel={onClose} onOk={onClose}>
+      <h1>User Modal</h1>
+    </Modal>
   );
+};
+
+const SomeHeader = () => {
+  const visibility = useToggleSelector("user-modal");
 
   useEffect(() => {
-    return () => {
-      toggleModule.removeModel("toggle");
-    };
-  }, []);
+    const unsubscribe = visibility?.subscribers.onChange((payload: any) => {
+      console.log("subscribe", visibility?.getVisibility(), payload);
+    });
+    return () => unsubscribe?.();
+  }, [visibility]);
 
   return (
     <div>
-      <h1>Status: {visibility}</h1>
-      <button onClick={() => model.commands.toggle()}>Toggle</button>
+      <h2>Header</h2>
+      <Button onClick={() => visibility?.commands.onOpen()}>
+        Open form header
+      </Button>
+    </div>
+  );
+};
+
+const SomeFooter = () => {
+  const visibilityCommands = useToggleCommands("user-modal");
+  return (
+    <div>
+      <h2>Footer</h2>
+      <Button onClick={() => visibilityCommands.onOpen()}>Open</Button>
+    </div>
+  );
+};
+
+const HomePage = () => {
+  return (
+    <div>
+      <SomeHeader />
+      <UserModal
+        model={{
+          id: "user-modal",
+          initState: "off",
+        }}
+      />
+      <SomeFooter />
     </div>
   );
 };
---
+++ test-app/src/toggle/types.ts
@@ -0,0 +1,39 @@
+export enum INITIAL_STATE {
+  ON = "on",
+  OFF = "off",
+}
+
+export type initialStateType = `${INITIAL_STATE}`;
+
+export type ToggleProps = {
+  id: string;
+  initState: initialStateType;
+};
+
+export interface IModelState {
+  visibility: initialStateType;
+}
+
+export interface IModelMutations {
+  setVisibility: (visibility: initialStateType) => void;
+}
+
+export interface IModelGetters {
+  getVisibility: () => initialStateType;
+}
+
+export interface ICommands {
+  onOpen: () => void;
+  onClose: () => void;
+  onToggle: () => void;
+}
+
+export interface ISubscribers {
+  onChange: (callback: (payload: initialStateType) => void) => () => void;
+}
+
+export interface IModelApiClient {
+  commands: ICommands;
+  subscribers: ISubscribers;
+  getVisibility: () => initialStateType;
+}
