14d4a7bd2af6eb980b95e764e52b2a526fc60727
medanmilos1831 <medanmilos1831@gmail.com>
2025-11-26 09:58:34 +0100

    Adjust HomePage demo text

--- test-app/src/pages/HomePage.tsx
+++ test-app/src/pages/HomePage.tsx
@@ -1,5 +1,35 @@
+import { useEffect } from "react";
+import { quantumUiReact } from "../quantum-ui-react";
+
+const counterModule = quantumUiReact.createModule({
+  name: "counter",
+  store: ({ id, state }) => ({
+    id,
+    state,
+  }),
+});
+
 const HomePage = () => {
-  return <></>;
+  const counter = counterModule.useEntitySelector("counter");
+  counterModule.useCreateEntity({ id: "counter", state: 0 });
+
+  useEffect(() => {
+    const unsubscribe = counter?.subscribe(({ prevState, newState }) => {
+      console.log(prevState, newState);
+    });
+    return () => {
+      unsubscribe?.();
+    };
+  }, [counter]);
+  console.log(counter);
+  return (
+    <div>
+      COUNTER
+      <button onClick={() => counter?.setState((state) => state + 1)}>
+        click me
+      </button>
+    </div>
+  );
 };
 
 export { HomePage };
