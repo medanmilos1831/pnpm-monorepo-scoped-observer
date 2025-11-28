a91e87b7df6d9a9b4cfd88f981dc24bb06ab1c32
medanmilos1831 <medanmilos1831@gmail.com>
2025-11-28 08:36:09 +0100

    Update toggle client implementation

--- test-app/src/toggle/createToggleClient.ts
+++ test-app/src/toggle/createToggleClient.ts
@@ -1,52 +1,68 @@
 import { useSyncExternalStore } from "react";
 import { quantumUiReact, type ISubscribe } from "../quantum-ui-react";
 
+enum toggleState {
+  ON = "on",
+  OFF = "off",
+}
+
+type toggleStateType = `${toggleState}`;
+
 interface IToggleClient {
   onOpen: () => void;
   onClose: () => void;
   onToggle: () => void;
-  getState: () => "on" | "off";
-  onChangeSubscriber: ISubscribe<"on" | "off">;
-  subscribe: ISubscribe<"on" | "off">;
+  getState: () => toggleStateType;
+  onChangeSubscriber: ISubscribe<toggleStateType>;
+  subscribe: ISubscribe<toggleStateType>;
 }
 const createToggleClient = () => {
-  const toggleModule = quantumUiReact.createModule<"on" | "off", IToggleClient>(
-    {
-      name: "toggle",
-      onCreateEntity: ({ id, state }: { id: string; state: "on" | "off" }) => {
-        return {
-          id,
-          state,
-        };
-      },
-      clientSchema: (store) => {
-        return {
-          onOpen() {
-            store.setState(() => "on");
-          },
-          onClose: () => {
-            store.setState(() => "off");
-          },
-          onToggle: () => {
-            store.setState((prevState) => {
-              return prevState === "on" ? "off" : "on";
-            });
-          },
-          getState: () => {
-            return store.getState();
-          },
-          onChangeSubscriber: (notify: () => void) => {
-            return store.subscribe((payload) => {
-              notify();
-            });
-          },
-          subscribe: store.subscribe,
-        };
-      },
-    }
-  );
+  const toggleModule = quantumUiReact.createModule<
+    toggleStateType,
+    IToggleClient
+  >({
+    name: "toggle",
+    onCreateEntity: ({ id, state }: { id: string; state: toggleStateType }) => {
+      return {
+        id,
+        state,
+      };
+    },
+    clientSchema: (store) => {
+      return {
+        onOpen() {
+          store.setState(() => toggleState.ON);
+        },
+        onClose: () => {
+          store.setState(() => toggleState.OFF);
+        },
+        onToggle: () => {
+          store.setState((prevState) => {
+            return prevState === toggleState.ON
+              ? toggleState.OFF
+              : toggleState.ON;
+          });
+        },
+        getState: () => {
+          return store.getState();
+        },
+        onChangeSubscriber: (notify: () => void) => {
+          return store.subscribe((payload) => {
+            notify();
+          });
+        },
+        subscribe: store.subscribe,
+      };
+    },
+  });
   return {
-    useToggle: ({ id, initState }: { id: string; initState: "on" | "off" }) => {
+    useToggle: ({
+      id,
+      initState,
+    }: {
+      id: string;
+      initState: toggleStateType;
+    }) => {
       toggleModule.useCreateEntity({ id, state: initState });
       const model = toggleModule.getEntityById(id)!;
       const visibility = useSyncExternalStore(
