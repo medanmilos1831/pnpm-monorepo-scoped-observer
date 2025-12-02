41eee0125cbc2fb89413aee8f380ae1276ffd856
medanmilos1831 <medanmilos1831@gmail.com>
2025-12-02 10:40:29 +0100

    Simplify HomePage: cleanup and refactor

--- test-app/src/pages/HomePage.tsx
+++ test-app/src/pages/HomePage.tsx
@@ -1,123 +1,46 @@
-import { Button, Drawer, Modal } from "antd";
-import { useState } from "react";
 import { createReactToggleObserver } from "../toggle/react-toogle-observer";
+import { Button, Modal } from "antd";
 
 const toggleObservers = createReactToggleObserver({
-  modal: {
+  userModal: {
     logger: (params) => {
       console.log("MODAL LOGGER", params);
     },
   },
-  drawer: {
+  authModal: {
     logger: (params) => {
       console.log("DRAWER LOGGER", params);
     },
   },
 });
-const { modal, drawer } = toggleObservers;
-const { useToggle: useModalToggle, useInterceptor: useModalInterceptor } =
-  modal;
+const { userModal, authModal } = toggleObservers;
+const { useToggle: useUserModal, useInterceptor: useUserModalInterceptor } =
+  userModal;
 
-const { useToggle: useDrawerToggle, useInterceptor: useDrawerInterceptor } =
-  drawer;
+const { useToggle: useAuthToggle, useInterceptor: useAuthInterceptor } =
+  authModal;
 
-const ModalComponent = () => {
-  const [counter, setCounter] = useState(0);
-  const [value, close, message] = useModalToggle(false);
+const UserModalComponent = () => {
+  const [value, close, message] = useUserModal(false);
   return (
-    <>
-      <Button onClick={() => setCounter(counter + 1)}>Increment</Button>
-      <p>Counter: {counter}</p>
-      <Modal
-        open={value}
-        onCancel={() =>
-          close({
-            name: "close message",
-          })
-        }
-      >
-        <h1>Modal Content</h1>
-      </Modal>
-    </>
+    <Modal open={value} onCancel={() => close()} onOk={() => close()}>
+      <h1>User Modal</h1>
+    </Modal>
   );
 };
-
-const DrawerComponent = () => {
-  const [value, close, message] = useDrawerToggle(false);
-  console.log("DRAWER MESSAGE", value);
-  return (
-    <>
-      <Drawer
-        open={value}
-        onClose={() =>
-          close({
-            name: "close message",
-          })
-        }
-      >
-        <h1>Modal Content</h1>
-      </Drawer>
-    </>
-  );
-};
-
 const SomeComponent = () => {
-  const [counter, setCounter] = useState(0);
-  useDrawerInterceptor((payload: any) => {
-    return {
-      ...payload,
-      counter: counter + 1,
-    };
-  }, "open");
-  useModalInterceptor((payload: any) => {
-    return {
-      ...payload,
-      counter: counter + 12,
-    };
-  }, "open");
-  return (
-    <div>
-      <h1>SomeComponent</h1>
-      <Button onClick={() => setCounter(counter + 1)}>Increment</Button>
-      <p>Counter: {counter}</p>
-    </div>
-  );
-};
-
-const ButtonComponent = () => {
   return (
     <>
-      <Button
-        onClick={() => {
-          toggleObservers.modal.open({
-            name: "John Doe",
-          });
-        }}
-      >
-        Open Modal
-      </Button>
-      <Button
-        onClick={() => {
-          toggleObservers.drawer.open({
-            name: "John Doe",
-          });
-        }}
-      >
-        Open Drawer
-      </Button>
+      <Button onClick={() => userModal.open()}>Open Modal</Button>
     </>
   );
 };
-
 const HomePage = () => {
   return (
-    <div>
+    <>
+      <UserModalComponent />
       <SomeComponent />
-      <h1>HomePage</h1>
-      <ModalComponent />
-      <DrawerComponent />
-      <ButtonComponent />
-    </div>
+    </>
   );
 };
 
